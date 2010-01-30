39f5683bcb40f9739e3f7f63c31bcaf9129ee37e
Thomas Misund <thomas@hemmeligadresse.com>
2010-01-30 15:08:36 +0100

    Oppdaterer utseendet til å matche studentersamfundet-web 1.1 bedre.

--- inside/Page.php
+++ inside/Page.php
@@ -27,62 +27,87 @@ class Page {
   public function display(){
     $this->_displayHeader();
     ?>
-  <body>
+  <body class="dns section-inside">
     <div id="container">
       <div id="header">
         <span class="site-title"><a href="http://studentersamfundet.no/"><?php print $this->page_title; ?></a></span>
 
+        <div id="quick-menu">
+            Om Studentersamfundet | <a href="http://studentersamfundet.no/kart.php">Kart</a> | <a href="http://studentersamfundet.no/kontakt.php">Kontakt</a>
+        </div>
+
+        <div class="aapningstider"> 
+            <table> 
+                <tr><td>Mandag-onsdag</td><td>13-01</td></tr> 
+                <tr><td>Torsdag-fredag</td><td>13-03</td></tr> 
+                <tr><td>L&oslash;rdag</td><td>16-03</td></tr> 
+                <tr><td>Kjøkkenet</td><td>Til 19</td></tr> 
+            </table> 
+        </div>
+
 <?php /*
-This menu thing is pretty much copied and pasted from functions.php. Could we possibly include it instead? --Thomas Misund, 26. Oct 2009
+This menu thing is pretty much copied and pasted from functions.php in the studentersamfundet-web project. Could we
+possibly include it instead? --Thomas Misund, 26. Oct 2009
  */ ?>
         <div id="menu">
             <ul>
+                <li id="forside-meny"><a href="http://studentersamfundet.no/">Forside</a>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="http://studentersamfundet.no/lokaler.php">Lokaler</a></li>
+                            <li><a href="http://studentersamfundet.no/historie.php">Historie</a></li>
+                            <li><a href="http://studentersamfundet.no/billetter.php">Billetter</a></li>
+                            <li><a href="http://studentersamfundet.no/medlemmer.php">Bli medlem</a></li>
+                            <li><a href="http://studentersamfundet.no/medlem/index.php">Registrere medlemskort</a></li>
+                        </ul>
+                    </div>
+                </li>
                 <li id="program-meny"><a href="http://studentersamfundet.no/prog.php">Program</a>
-                    <ul>
-                        <li><!--a href="http://studentersamfundet.no/prog.php">Alle</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=konsert">Konsert</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=debatt">Debatt</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=film">Film</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=fest">Fest</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=teater">Teater</a></li>
-                        <li><a href="http://studentersamfundet.no/prog.php?type=annet">Annet</a></li>
-                        <li><a href="http://studentersamfundet.no/konsepter.php">Konsepter</a></li>
-                        <li><a href="http://studentersamfundet.no/booking.php">Booking</a--> </li>
-                    </ul>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="http://studentersamfundet.no/prog.php">Alle</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=konsert">Konsert</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=debatt">Debatt</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=film">Film</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=fest">Fest</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=teater">Teater</a></li>
+                            <li><a href="http://studentersamfundet.no/prog.php?type=annet">Annet</a></li>
+                            <li><a href="http://studentersamfundet.no/konsepter.php">Konsepter</a></li>
+                            <li><a href="http://studentersamfundet.no/booking.php">Booking</a></li>
+                        </ul>
+                    </div>
                 </li>
-                <li id="informasjon-meny"><a href="http://studentersamfundet.no/lokaler.php">Informasjon</a>
-                    <ul>
-                        <li><!--a href="http://studentersamfundet.no/lokaler.php">Huset</a></li>
-                        <li><a href="http://studentersamfundet.no/foreninger.php">Foreninger</a></li>
-                        <li><a href="http://studentersamfundet.no/historie.php">Historie</a></li>
-                        <li><a href="http://studentersamfundet.no/jobb.php">Jobb</a></li>
-                        <li><a href="http://studentersamfundet.no/billetter.php">Billetter</a></li>
-                        <li><a href="http://studentersamfundet.no/kart.php">Kart</a--> </li>
-                    </ul>
-                </li>
-                <li id="medlem-meny"><a href="http://studentersamfundet.no/medlemmer.php">Medlem</a>
-                    <ul>
-                        <li><!--a href="https://studentersamfundet.no/inside/index.php">Medlemsider</a></li>
-                        <li><a href="http://studentersamfundet.no/fordeler.php">Fordeler</a></li>
-                        <li><a href="http://studentersamfundet.no/medlemmer.php">Bli medlem</a></li>
-                        <li><a href="http://studentersamfundet.no/aktive.php">Aktive</a></li>
-                        <li><a href="/medlem/index.php">Registrere medlemskort</a--> </li>
-                    </ul>
+                <li id="foreninger-meny"><a href="http://studentersamfundet.no/foreninger.php">Foreninger</a>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="http://studentersamfundet.no/foreninger.php">Foreninger</a></li>
+                        </ul>
+                    </div>
                 </li>
                 <li id="forum-meny"><a href="http://studentersamfundet.no/forum/index.php">Forum</a>
-                    <ul>
-                        <li><!--Ingen undermenypunkter--> </li>
-                    </ul>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="http://studentersamfundet.no/forum/viewforum.php?f=68">Musikk</a></li>
+                            <li><a href="http://studentersamfundet.no/forum/viewforum.php?f=69">Debatt</a></li>
+                            <li><a href="http://studentersamfundet.no/forum/viewforum.php?f=70">Fritt forum</a></li>
+                        </ul>
+                    </div>
                 </li>
-                <li id="inside-meny" class="current"><a href="https://studentersamfundet.no/inside">Inside</a>
-                    <ul>
-                        <li><!--Ingen undermenypunkter--> </li>
-                    </ul>
+                <li id="inside-meny" class="current"><a href="https://www.studentersamfundet.no/inside">For medlemmer</a>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="https://www.studentersamfundet.no/inside/">Inside</a></li>
+                            <li><a href="http://studentersamfundet.no/viteboka/?page_id=3">Viteboka</a></li>
+                            <li><a href="https://www.studentersamfundet.no/inside/index.php?page=display-barshifts-calendar">Tappet&aring;rnets vaktliste</a>
+                            <li><a href="http://studentersamfundet.no/aktive.php">Andre ressurser</a></li>
+                        </ul>
+                    </div>
                 </li>
-                <li id="kontakt-meny"><a href="http://studentersamfundet.no/kontakt.php">Kontakt</a>
-                    <ul>
-                        <li><!--Ingen undermenypunkter--> </li>
-                    </ul>
+                <li id="nyheter-meny"><a href="http://studentersamfundet.no/nyheter.php">Nyheter</a>
+                    <div class="sub-menu">
+                        <ul>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </div> <!-- #menu -->
