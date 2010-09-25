c71d5f5b4cddb99c9768e3c3f5c7f3bc62ce87e3
oyvinbak <oyvinbak@portaoli>
2010-09-25 22:03:17 +0200

    Fixed redirect in index.php so that inside uses ssl

--- inside/index.php
+++ inside/index.php
@@ -1,5 +1,13 @@
 <?php
 
+$url = $_SERVER['SCRIPT_URI'];
+if(substr($url, 0, 5) != 'https')
+{
+	$newurl = str_replace('http://','https://',$url);
+	header("Location: $newurl");
+	die;
+}
+
 $time_start = microtime(true);
 
 require_once "includes.php";
