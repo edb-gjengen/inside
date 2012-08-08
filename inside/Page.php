c1fa8fd6610e5fe268c3482636f65558ea1adb59
Nikolai Kristiansen <nikolaik@gmail.com>
2012-08-08 13:13:06 +0200

    Updates register-user text

--- inside/Page.php
+++ inside/Page.php
@@ -2331,7 +2331,7 @@ Om dere lager en facebook side til arrangementet, ikke glem &aring; putte inn le
 
 		if (!isset($_SESSION['valid-user'])){?>
 		<h2>Bli medlem!</h2>
-		<p>Her kan du registrere deg for så å kjøpe medlemskap! Om du allerede har en konto kan du logge inn på <a href="/">Inside</a></p>
+		<p>Her kan du registrere ditt midlertidige medlemskap. Er du allerede registrert? <a href="/">Logg inn</a> først.</p>
 		<p>Alle feltene må fylles ut.</p>
 
 		<?php
